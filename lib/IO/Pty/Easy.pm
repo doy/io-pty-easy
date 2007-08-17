65dc022f687b3bf5016853b220033e266ed37a09
jluehrs2 <jluehrs2@uiuc.edu>
2007-08-16 19:00:36 -0500

    move the module into place, add some boilerplate pod stuff

--- lib/IO/Pty/Easy.pm
+++ lib/IO/Pty/Easy.pm
@@ -151,3 +151,86 @@ sub close {
 }
 
 1;
+#!perl
+package IO::Pty::Easy;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+IO::Pty::Easy - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use IO::Pty::Easy;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-io-pty-easy at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=IO-Pty-Easy>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc IO::Pty::Easy
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/IO-Pty-Easy>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/IO-Pty-Easy>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=IO-Pty-Easy>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/IO-Pty-Easy>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
